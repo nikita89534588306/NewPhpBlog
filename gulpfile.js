1b00c09debebf6405c4e2ef6e78cdc6f2139ae5d
Nikita <nikita.89534588306@google.com>
2023-02-11 11:04:41 +0300

    тестовый таск который берет файл переименовывае и кладет в директорию

--- gulpfile.js
+++ gulpfile.js
@@ -1,11 +1,14 @@
 var gulp = require("gulp")
+var rename = require("gulp-rename") //подключаем модуль переименования файлов 
 
 //тестовый таск 
 function copy(done){    
     gulp.src('./src/scss/main.scss') //берем файл по пути
-        .pipe(  //далее...
-            gulp.dest('./src/css/') //отправляем файл по пути './scr/css/' 
-        );
+        // pipe() функция в которую передаем колбек для выполнения 
+        // заданий последовательно 
+        .pipe(rename("main.css"))  //пеиминовываем файл
+        .pipe(gulp.dest('./src/css/') //отправляем файл по пути './scr/css/' 
+    );
     done();
 }
 
