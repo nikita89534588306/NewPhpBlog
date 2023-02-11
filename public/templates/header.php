4087b192e8552e35aa6717c3c4c5afba725071da
Nikita <nikita.89534588306@google.com>
2023-02-11 13:14:54 +0300

    исправленны ссылки для шапки

--- public/templates/header.php
+++ public/templates/header.php
@@ -5,14 +5,14 @@
 			</h2>
 
 			<ul class="navbar">
-				<li class="navbar-item"><a class="navbar-link" href="">Главная</a></li>
+				<li class="navbar-item"><a class="navbar-link" href="/">Главная</a></li>
 				<li class="navbar-item"><a class="navbar-link" href="">О нас</a></li>
 				<li class="navbar-item"><a class="navbar-link" href="">Услуги</a></li>
 				<li class="navbar-item user-select-list">
 					<a class="navbar-link" href=""><i class='fa fa-user icon'></i>Кабинет</a>
 					<ul class="user-action">
 						<li class="navbar-item"><a class="navbar-link" href="">Админ панель</a></li>
-						<li class="navbar-item"><a class="navbar-link" href="">Выход</a></li>
+						<li class="navbar-item"><a class="navbar-link" href="login.php">Выход</a></li>
 					</ul>
 				</li>
 				
