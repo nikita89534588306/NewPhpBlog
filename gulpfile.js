30f2b9b6a662d1fde3f4e0222b671c9d3be3e55a
Nikita <nikita.89534588306@google.com>
2023-02-11 12:17:27 +0300

    добавленна функция автопрефиксов

--- gulpfile.js
+++ gulpfile.js
@@ -1,7 +1,7 @@
 var gulp = require("gulp")
 var rename = require("gulp-rename") //подключаем модуль переименования файлов 
 const sass = require('gulp-sass')(require('sass')); //подключаем модуль препроцессора sass
-
+const autopre = require('gulp-autoprefixer') //подключаем модуль автопрефиксера
 
 function sassToCSS(done){    
     gulp.src('./src/scss/main.scss') //берем файл по пути
@@ -15,6 +15,14 @@ function sassToCSS(done){
             )
         )
         .on("error", console.error.bind(console)) //ОБРАБОТЧИК СОБЫТИЙ - БЕЗ НЕГО НЕ ВЫВОДЯТСЯ ОШИБКИ ОТ ПРЕПРОЦЕССОТА SASS
+        .pipe(
+            autopre(                                    //функция модуля автопрефиксера                                
+                {
+                    overrideBrowserslist: ['last 4 versions'],     // последние 10 версий
+                     cascade: false
+                }
+            )
+        )
         .pipe(
             rename(                             //функция для переименования файла
                 {suffix: ".min"},                   //добавление суффикса
