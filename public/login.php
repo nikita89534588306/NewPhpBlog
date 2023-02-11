7a7f00cabcb2324fb1e50cbb8373f3047599fcae
Nikita <nikita.89534588306@google.com>
2023-02-11 13:07:06 +0300

    создана страница с регистрацией

---
+++ public/login.php
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <?php include "./templates/head.php" ?>
+</head>
+<body>
+    <?php include "./templates/header.php" ?>
+    <?php include "./templates/footer.php" ?>
+</body>
+</html>
