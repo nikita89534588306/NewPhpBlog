90f05e7c04f6832a08776d5da3e58756f15e462c
Nikita Balashov <nikita.89534588306@gmail.com>
2023-02-13 17:18:18 +0500

    обновлен файл регистрации пользователей

--- localhost/app/database/controllers/users.php
+++ localhost/app/database/controllers/users.php
@@ -20,14 +20,19 @@
 		else if($user_password !== $passwordConfirm)
 			$errMsg = "Пароли не совпадают";
 		else{
+			//проверяем есть ли в БД данные емайл
 			$isNotUniqueEmail = queryDB("SELECT id FROM users WHERE email = '$email' LIMIT 1")->fetch();
+			//если есть выводим ошибку
 			if($isNotUniqueEmail){
 				$errMsg = "Пользователь с данной почтой уже зарегестрирован!";
 			}
 			else{
 				//хешируем пароль
 				$user_password = password_hash($user_password, PASSWORD_DEFAULT);		//хешируем пароль
+				//заносим в базу данных
 				queryDB("INSERT INTO webPhp.users (username, email, user_password) VALUES ( '$login', '$email', '$user_password');");  //добавляем пользователя в БД
+
+				//... ДАЛЕЕ ИДЕТ ЛОГИКА РАБОТЫ С СЕССИЯМИ ...
 			}
 		}		
 	}
