6c697acb899ae2ffbc48bd6a7e99464626565b00
Nikita Balashov <nikita.89534588306@gmail.com>
2023-02-13 15:53:34 +0500

    добавлена валидация длинный логина и проверка подтверждения пароля

--- localhost/app/database/controllers/users.php
+++ localhost/app/database/controllers/users.php
@@ -13,10 +13,12 @@
 		/*проверка заполнены ли поля...*/ 
 		if( $login === ''|| $email === '' || $user_password === '' || $passwordConfirm === '')
 				$errMsg = "Не все поля заполнены";
-		// else if(){
-
-			
-		// }
+		// проверяем логин - не короче 2х символов		
+		else if(mb_strlen($login, 'UTF8') < 2)
+			$errMsg = "Логин не может быть короче 2-х символов";
+		//проверка подтверждения пароля
+		else if($user_password !== $passwordConfirm)
+			$errMsg = "Пароли не совпадают";
 		else{
 			// $user_password = password_hash($user_password, PASSWORD_DEFAULT);		//хешируем пароль
 			// queryDB("INSERT INTO webPhp.users (username, email, user_password) VALUES ( '$login', '$email', '$user_password');");  //добавляем пользователя в БД
