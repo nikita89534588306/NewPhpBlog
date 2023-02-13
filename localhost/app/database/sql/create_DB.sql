96743107e03c465359f5f8677a1e681f1f27f583
Nikita Balashov <nikita.89534588306@gmail.com>
2023-02-13 14:43:05 +0500

    созданна таблица с ролями пользователей

--- localhost/app/database/sql/create_DB.sql
+++ localhost/app/database/sql/create_DB.sql
@@ -31,3 +31,19 @@ VALUES
 ( 'Лёша', 'ALEX007@mail.ru', '$2y$10$JhLnX.EhidrcFZjvktYGt.3jQrv94THERB3mGUChF2oLUH2g5Ors.'), /*assHole*/
 ( 'xxx', 'xxx@ass.com', '$2y$10$srZi7f3IkT.hnaIvBv9FK.N4QhlZxpXN3duPyK3GrzpsvZSvio4Hq'), /*qwerty*/
 ( 'Mary', 'bahama@mail.ru', '$2y$10$IjWowhPwd3lbjt5I/Ci.fee309ceM4IfEQPFiRmUF5q1KMn3X.OI6'); /*bagama*/
+
+/*создаем таблицу с ролями пользователей*/
+CREATE TABLE IF NOT EXISTS webPhp.roleVariants(
+	id INT PRIMARY KEY AUTO_INCREMENT NOT NULL,
+	name_role VARCHAR(100) NOT NULL
+)
+/*добавляем роли пользователей*/
+INSERT INTO webPhp.roleVariants (id, name_role) 
+VALUES 
+(1, 'user'),
+(2, 'admin'); 
+
+/*добавляем колонку с ролями пользователей (по умолчанию 1 - user)*/
+ALTER TABLE users 
+ADD user_role INT DEFAULT(1) REFERENCES roleVariants(id);
+
