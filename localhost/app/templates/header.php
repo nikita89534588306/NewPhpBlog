df9933225b96841f5e5f6128cc954d74647fb409
Nikita Balashov <nikita.89534588306@gmail.com>
2023-02-14 17:16:31 +0500

    исправлена шапка и стили шапки

--- localhost/app/templates/header.php
+++ localhost/app/templates/header.php
@@ -9,10 +9,10 @@
 				<li class="navbar-item"><a class="navbar-link" href="">О нас</a></li>
 				<li class="navbar-item"><a class="navbar-link" href="">Услуги</a></li>
 				<li class="navbar-item user-select-list">
-					<a class="navbar-link" href=""><i class='fa fa-user icon'></i>Кабинет</a>
+					<a class="navbar-link" href=""><i class='fa fa-user icon'></i>Пользователь</a>
 					<ul class="user-action">
 						<li class="navbar-item"><a class="navbar-link" href="">Админ панель</a></li>
-						<li class="navbar-item"><a class="navbar-link" href="login.php">Выход</a></li>
+						<li class="navbar-item"><a class="navbar-link" href="login.php">Выход из аккаунта</a></li>
 					</ul>
 				</li>
 				
