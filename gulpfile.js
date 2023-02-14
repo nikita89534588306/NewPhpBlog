168a48108c4317d4d9b7aa1ce90dd58e49261028
Nikita Balashov <nikita.89534588306@gmail.com>
2023-02-14 11:17:02 +0500

    настроенно хештрование css файла

--- gulpfile.js
+++ gulpfile.js
@@ -2,6 +2,8 @@ var gulp = require("gulp")
 var rename = require("gulp-rename") //подключаем модуль переименования файлов 
 const sass = require('gulp-sass')(require('sass')); //подключаем модуль препроцессора sass
 const autopre = require('gulp-autoprefixer') //подключаем модуль автопрефиксера
+var hash = require('gulp-hash'); //ДОБАВЛЕНИЕ ХЕША ДЛЯ ФАЙЛА
+
 
 function sassToCSS(done){    
     gulp.src('./scss/**/*', {nodir: true}) //берем файл по пути
@@ -29,11 +31,24 @@ function sassToCSS(done){
                 "main.css"                          //имя файла
             )
         )  
+		//ДОБАВЛЕНИЕ ХЕША ДЛЯ ФАЙЛА
+		.pipe(hash()) // добавляем хеш к файлу
         .pipe(
             gulp.dest(          //отправляем файл по пути ...
-                './public/css/'    // './scr/css/' 
+                 './localhost/app/css'    // './scr/css/' 
             ) 
-    );
+		)
+
+		
+		//далее каждый раз мы должны записывать файл манифест
+		.pipe(hash.manifest('./assets.json', { // создать файл манифеста и папки указанные в пути
+			deleteOld: true,					//обновлять версии ХЕША - да
+			sourceDir: './localhost/app/css' 	//файл с которым работаем находится в дириктории ...
+			}))
+		.pipe(
+			gulp.dest('.') // записать файл манифеста
+		); 
+
     done();
 }
 
